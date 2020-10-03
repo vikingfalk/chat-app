f2e9a4d4613b169a9127589c12ddefa500e9c027
Viking Falk Pettersson <vikingfalk@gmail.com>
2020-10-04 01:14:08 +0200

    first commit

---
+++ public/js/room.js
@@ -0,0 +1,64 @@
+// Connect to socket
+const socket = io('http://localhost:3000');
+
+
+// Get DOM elements
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+const roomUsers = document.getElementById('room-users');
+
+
+// Initialize new user
+socket.emit('new-user', roomName, userName);
+
+
+// Event listeners
+messageForm.addEventListener('submit', sendChatMessage);
+
+
+// Listen for socket events
+socket.on('chat-message', appendMessage);
+socket.on('user-connected', handleUserConnected);
+socket.on('user-disconnected', handleUserDisconnected);
+
+
+// Function declarations
+function sendChatMessage(event) {
+    event.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-chat-message', roomName, message);
+    messageInput.value = '';
+}
+
+function appendMessage(message, classes) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    classes.forEach(cls => messageElement.classList.add(cls));
+
+    messageElement.innerHTML = message;
+
+    messageContainer.append(messageElement);
+
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function handleUserConnected(message, name) {
+    appendMessage(message, ['recieved', 'server-msg']);
+    
+    const userElement = document.createElement('li');
+    userElement.classList.add('user');
+    userElement.setAttribute('id', name);
+
+    userElement.innerHTML = `
+    <span class="material-icons">person</span>
+    ${name}`;
+
+    roomUsers.append(userElement);
+}
+
+function handleUserDisconnected(message, name) {
+    appendMessage(message, ['recieved', 'server-msg']);
+
+    roomUsers.removeChild(roomUsers.children.namedItem(name));
+}
---
+++ server.js
@@ -0,0 +1,90 @@
+const moment = require('moment');
+
+// Setup server
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+const rooms = {};
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { rooms });
+});
+
+app.get('/room', (req, res) => {
+    const room = req.query.room;
+    const username = req.query.username;
+
+    if(rooms[room] == null) {
+        return res.redirect('/');
+    }
+
+    res.render('room', { roomName: room, userName: username,
+        users: Object.values(rooms[room].users) });
+});
+
+app.post('/room', createNewRoom);
+
+
+// Listen for socket events
+io.on('connection', socket => {
+    socket.on('new-user', (room, name) => {
+        socket.join(room);
+        rooms[room].users[socket.id] = name;
+
+        const formattedMsg = formatMessage(`${name} joined the room`, 'ChatBot');
+        io.to(room).emit('user-connected', formattedMsg, name);
+    });
+
+    socket.on('send-chat-message', (room, message) => {
+        const formattedMsg = formatMessage(message, rooms[room].users[socket.id]);
+        socket.to(room).broadcast.emit('chat-message', formattedMsg, ['recieved']);
+        socket.emit('chat-message', formattedMsg, ['sent']);
+    });
+
+    socket.on('disconnect', () => {
+        getUserRooms(socket).forEach(room => {
+            const name = rooms[room].users[socket.id];
+            const formattedMsg = formatMessage(`${name} left the room`, 'ChatBot');
+            socket.to(room).broadcast.emit('user-disconnected', formattedMsg, name);
+            delete rooms[room].users[socket.id];
+        });
+    });
+});
+
+
+// Function declarations
+function createNewRoom(req, res) {
+    const newRoom = req.body.room;
+
+    if(rooms[newRoom] != null) {
+        return res.redirect('/');
+    }
+
+    rooms[newRoom] = { users: {} };
+    res.redirect('/');
+}
+
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if(room.users[socket.id] != null) names.push(name);
+        return names;
+    }, []);
+}
+
+function formatMessage(message, name) {
+    return `<span class="name">${name}</span>
+            <span class="time">${moment().format('HH:mm')}</span>
+            <p>${message}</p>`;
+}
